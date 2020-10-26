103205b08f03cda15d0d11c279b94d3daa8818bc
Claudeegarcia7 <Claudeegarcia@icloud.com>
2020-10-26 00:33:52 -0500

    Done.

--- lib/catch_phrase.rb
+++ lib/catch_phrase.rb
@@ -1,3 +1,10 @@
 def catch_phrase
   puts phrase
 end
+
+describe "#catch_phrase_=" do
+it "puts out a catch phrase" do
+  phrase = "It's-a me, Mario!"
+  expect {catch_phrase}.to output("It's-a me, Mario!\n").to_stdout
+end
+end
