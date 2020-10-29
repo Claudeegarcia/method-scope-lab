7746663c2c5f6f80d74c0a9305746475d9380ca7
Claudeegarcia7 <Claudeegarcia@icloud.com>
2020-10-29 15:32:32 -0500

    commit tree fbba20fba201d344a036097a1739d177de68ff35

--- lib/catch_phrase.rb
+++ lib/catch_phrase.rb
@@ -1,10 +1,22 @@
-def catch_phrase
+#catch_phrase = puts out a catch phrase
+
+require "spec_helper"
+describe "#catch_phrase" do
+it "puts out a catch phrase" do
+  phrase = "It's-a me, Mario!"
+  expect{catch_phrase(phrase)}.to output("It's-a me, Mario!\n").to_stdout
+end
+end
+
+def catch_phrase (phrase)
   puts phrase
 end
 
-describe "#catch_phrase_=" do
-it "puts out a catch phrase" do
-  phrase = "It's-a me, Mario!"
-  expect {catch_phrase}.to output("It's-a me, Mario!\n").to_stdout
-end
+require "spec_helper"
+
+describe "#catch_phrase" do
+  it "puts out a catch phrase" do
+    phrase = "It's-a me, Mario!"
+    expect{catch_phrase(phrase)}.to output("It's-a me, Mario!\n").to_stdout
+  end
 end
